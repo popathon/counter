3d04e43f9db746d05114745c5651d6eafcede54e
basilesimon <basile@basilesimon.fr>
2015-02-27 23:11:58 +0000

    Fixed the maths

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var date = new Date(2015, 2, 28, 0,0,0,0);
+var date = new Date(2015, 1, 28, 18, 0, 0, 0);
 var today = new Date();
 var dif = date.getTime() - today.getTime();
-var timeLeft = Math.abs(dif/1000)/60;
+var timeLeft = Math.abs(dif/1000);
 
-var clock = $('.your-clock').FlipClock({
+var clock = $(".your-clock").FlipClock({
   countdown: true
 });
 clock.setTime(timeLeft);

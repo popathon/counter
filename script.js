25b071db5955e9c6806abed367967ad36be10eeb
basilesimon <basile@basilesimon.fr>
2015-02-27 19:48:06 +0000

    All working

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-var clock = $('.your-clock').FlipClock(3600, {
+var date = new Date(2015, 2, 28, 0,0,0,0);
+var today = new Date();
+var dif = date.getTime() - today.getTime();
+var timeLeft = Math.abs(dif/1000)/60;
+
+var clock = $('.your-clock').FlipClock({
   countdown: true
 });
+clock.setTime(timeLeft);
+clock.start();
